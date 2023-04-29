a0a35eaff9a9172d601eeafeea7c685aebf8daef
loto99win <60571321+loto99win@users.noreply.github.com>
2023-04-29 18:37:09 +0700

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dpkqnwp9v',
+  api_key: '436481897595333',
+  api_secret: 'v_D-gGLSdmr71h2f7BlVfs1Hh1E',
 });
 
 module.exports = cloudinary;
